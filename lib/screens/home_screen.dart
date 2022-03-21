04625a3c45af6752f38e6d8c1942a8ec800ac773
timbaton <timbaton@users.noreply.github.com>
2022-03-21 23:59:10 +0400

    Add suggestions widget.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
-
+import 'package:bank_lite/components/suggestions.dart';
+import 'package:bank_lite/generated/assets.gen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -13,8 +14,51 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.center,
-      child: CupertinoButton(child: const Text("123132123"), onPressed: () { })
+        color: const Color(0xFF222222),
+        child: Column(
+          children: [const SizedBox(height: 70), SuggestionWidget()],
+        ));
+  }
+}
+
+class SuggestionWidget extends StatefulWidget {
+  @override
+  State<SuggestionWidget> createState() {
+    return _SuggestionsState();
+  }
+}
+
+class _SuggestionsState extends State<SuggestionWidget> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final items = [
+      SuggestionItem(
+          title: "Пригласить друга",
+          content: Assets.lib.assets.images.serviceLike),
+      SuggestionItem(
+          title: "Оплатить сотовую связь",
+          content: Assets.lib.assets.images.serviceDone),
+      SuggestionItem(
+          title: "Перевести деньги",
+          content: Assets.lib.assets.images.serviceNext),
+      SuggestionItem(
+          title: "Оплатить услуги ЖКХ",
+          content: Assets.lib.assets.images.serviceHome),
+    ];
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Suggestions(
+        items: items,
+        selectedIndex: _selectedIndex,
+        onPressed: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
     );
   }
 }
