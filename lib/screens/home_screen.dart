7bb262b8ceb3818b496f3eacad0092a81e9d7021
ton252 <tonwork252@gmail.com>
2022-03-22 11:58:46 +0300

    Add list builder

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,14 @@
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+abstract class HomeScreenModel {
+  //
+}
+
+class BannerScreenModel extends HomeScreenModel {
+  int index;
+  BannerScreenModel({required this.index});
+}
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,11 +18,39 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List<HomeScreenModel> _items = [
+    BannerScreenModel(index: 0),
+    BannerScreenModel(index: 1),
+    BannerScreenModel(index: 2),
+    BannerScreenModel(index: 3),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
-      child: CupertinoButton(child: const Text("123132123"), onPressed: () { })
+      child: ListView.builder(
+        physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+        itemCount: _items.length,
+        itemBuilder: (ctx, index) {
+          final item = _items[index];
+          if (item is BannerScreenModel) {
+            switch (item.index) {
+              case 0:
+                return Container(height: 50.0, width: double.infinity, color: Colors.black87);
+              case 1:
+                return Container(height: 50.0, width: double.infinity, color: Colors.green);
+              case 2:
+                return Container(height: 50.0, width: double.infinity, color: Colors.red);
+              case 3:
+                return Container(height: 50.0, width: double.infinity, color: Colors.blue);
+            }
+            return Container();
+          } else {
+            return Container();
+          }
+        }
+      )
     );
   }
 }
