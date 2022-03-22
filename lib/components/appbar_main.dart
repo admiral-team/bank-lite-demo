e47909eaabb9f8066b1ed7a84fb461123d663ebd
Алмазов Иван Александрович <meaplasma@gmail.com>
2022-03-22 11:18:36 +0300

    Create appBar

---
+++ lib/components/appbar_main.dart
@@ -0,0 +1,51 @@
+import 'package:bank_lite/generated/assets.gen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AppBarMain extends StatefulWidget {
+  SvgGenImage get title => Assets.lib.assets.images.vtbTitle;
+  SvgGenImage get leftBarButton => Assets.lib.assets.images.profile;
+  SvgGenImage get rightBarButton => Assets.lib.assets.images.alert;
+
+  final Function()? onPressedLeftButton;
+  final Function()? onPressedRightButton;
+
+  const AppBarMain(
+      {Key? key, this.onPressedLeftButton, this.onPressedRightButton})
+      : super(key: key);
+
+  @override
+  _AppBarState createState() => _AppBarState();
+}
+
+class _AppBarState extends State<AppBarMain> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Color.fromARGB(82, 6, 5, 34),
+        height: 48.0,
+        child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Padding(padding: EdgeInsets.only(left: 8)),
+          CupertinoButton(
+            minSize: 0,
+            padding: const EdgeInsets.all(0),
+            alignment: Alignment.bottomRight,
+            pressedOpacity: 1.0,
+            child: widget.leftBarButton.svg(height: 32, color: Colors.blue),
+            onPressed: widget.onPressedRightButton,
+          ),
+          Spacer(),
+          widget.title.svg(height: 20),
+          Spacer(),
+          CupertinoButton(
+            minSize: 0,
+            padding: const EdgeInsets.all(0),
+            alignment: Alignment.bottomRight,
+            pressedOpacity: 1.0,
+            child: widget.rightBarButton.svg(height: 32, color: Colors.blue),
+            onPressed: widget.onPressedRightButton,
+          ),
+          Padding(padding: EdgeInsets.only(right: 8)),
+        ]));
+  }
+}
--- lib/screens/root_screen.dart
+++ lib/screens/root_screen.dart
@@ -1,10 +1,11 @@
 import 'package:bank_lite/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import '../components/airbar.dart';
+import '../components/appbar_main.dart';
 import '../generated/assets.gen.dart';
 
 class RootScreen extends StatefulWidget {
-  const RootScreen({Key? key }) : super(key: key);
+  const RootScreen({Key? key}) : super(key: key);
 
   @override
   State<RootScreen> createState() => _RootScreenState();
@@ -16,9 +17,12 @@ class _RootScreenState extends State<RootScreen> {
   @override
   Widget build(BuildContext context) {
     final items = [
-      AirbarItem(title: "Главный", content: Assets.lib.assets.images.appbarMain),
-      AirbarItem(title: "Платежи", content: Assets.lib.assets.images.appbarPayments),
-      AirbarItem(title: "Услуги", content: Assets.lib.assets.images.appbarServices),
+      AirbarItem(
+          title: "Главный", content: Assets.lib.assets.images.appbarMain),
+      AirbarItem(
+          title: "Платежи", content: Assets.lib.assets.images.appbarPayments),
+      AirbarItem(
+          title: "Услуги", content: Assets.lib.assets.images.appbarServices),
       AirbarItem(title: "Чат", content: Assets.lib.assets.images.appbarChat),
     ];
 
@@ -42,21 +46,18 @@ class _RootScreenState extends State<RootScreen> {
 
   Widget _currentScreen() {
     switch (_selectedIndex) {
-    case 0:
-      return const HomeScreen();
-    case 1:
-      return Container(color: Colors.orange);
-    case 2:
-      return Container(color: Colors.green);
-    case 3:
-      return Container(color: Colors.red);
-    case 4:
-      return Container(color: Colors.blue);
+      case 0:
+        return const HomeScreen();
+      case 1:
+        return const AppBarMain();
+      case 2:
+        return Container(color: Colors.green);
+      case 3:
+        return Container(color: Colors.red);
+      case 4:
+        return Container(color: Colors.blue);
     }
 
     throw Exception('Screen not found');
   }
 }
-
-
-
