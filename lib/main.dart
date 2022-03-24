83d120f4f1496e1f3c562824fed58b874ed49d5a
timbaton <timbaton@users.noreply.github.com>
2022-03-24 12:42:51 +0400

    Add flutter secured storage.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,29 @@
-import 'package:bank_lite/screens/home_screen.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'screens/root_screen.dart';
-import 'generated/assets.gen.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-void main() {
+import 'screens/root_screen.dart';
+
+Future<void> main() async {
   runApp(const Application());
+
+  const storage = FlutterSecureStorage();
+  storage.delete(key: "e");
+  storage.delete(key: "p");
+
+  const email = "email";
+  const password = "1234";
+
+  await storage.write(key: "e", value: email);
+  await storage.write(key: "p", value: password);
+
+  final mail = await storage.read(key: "e");
+  final pass = await storage.read(key: "p");
+
+  if (kDebugMode) {
+    print("mail = $mail, pass = $pass");
+  }
 }
 
 class Application extends StatelessWidget {
@@ -21,38 +39,34 @@ class Application extends StatelessWidget {
         DefaultCupertinoLocalizations.delegate,
       ],
       theme: CupertinoThemeData(brightness: Brightness.light),
-      home: Scaffold(
-          body: RootScreen(),
-          resizeToAvoidBottomInset: false
-      ),
+      home: Scaffold(body: RootScreen(), resizeToAvoidBottomInset: false),
     );
   }
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   return CupertinoApp(
-  //     debugShowCheckedModeBanner: false,
-  //     localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
-  //       DefaultMaterialLocalizations.delegate,
-  //       DefaultWidgetsLocalizations.delegate,
-  //       DefaultCupertinoLocalizations.delegate,
-  //     ],
-  //     theme: const CupertinoThemeData(brightness: Brightness.light),
-  //     home: Scaffold(
-  //         body: Container(
-  //           alignment: Alignment.center,
-  //           color: Colors.white, 
-  //           child: Row(
-  //             mainAxisAlignment: MainAxisAlignment.center,
-  //             children: [
-  //               Assets.lib.assets.images.alert.svg(color: Colors.red),
-  //               Assets.lib.assets.images.arrowUp.svg(color: Colors.grey),
-  //               Assets.lib.assets.images.appbarMain.svg(color: Colors.blue),
-  //               Assets.lib.assets.images.vtbTitle.svg(color: Colors.black)
-  //           ])
-  //         ),
-  //         resizeToAvoidBottomInset: false),
-  //   );
-  // }
+// @override
+// Widget build(BuildContext context) {
+//   return CupertinoApp(
+//     debugShowCheckedModeBanner: false,
+//     localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
+//       DefaultMaterialLocalizations.delegate,
+//       DefaultWidgetsLocalizations.delegate,
+//       DefaultCupertinoLocalizations.delegate,
+//     ],
+//     theme: const CupertinoThemeData(brightness: Brightness.light),
+//     home: Scaffold(
+//         body: Container(
+//           alignment: Alignment.center,
+//           color: Colors.white,
+//           child: Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Assets.lib.assets.images.alert.svg(color: Colors.red),
+//               Assets.lib.assets.images.arrowUp.svg(color: Colors.grey),
+//               Assets.lib.assets.images.appbarMain.svg(color: Colors.blue),
+//               Assets.lib.assets.images.vtbTitle.svg(color: Colors.black)
+//           ])
+//         ),
+//         resizeToAvoidBottomInset: false),
+//   );
+// }
 }
-
